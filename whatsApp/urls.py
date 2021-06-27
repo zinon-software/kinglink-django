d424ab9b7a40ed23ad9e909e97480220a1715be3
zinon <sawh2030@gmail.com>
2021-06-27 14:47:58 +0200

    baild all group html

--- whatsApp/urls.py
+++ whatsApp/urls.py
@@ -4,7 +4,7 @@ from . import views
 from . import api
 
 urlpatterns = [
-    path('', views.all_group),
+    path('', views.all_group, name='all_group'),
 
     path('api/Groub', api.GroubApi.as_view(), name='GroubApi'),
     path('api/Groub/<int:id>', api.GroubDetailApi.as_view(), name='GroubDetailApi'),
--- whatsApp/views.py
+++ whatsApp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def all_group(request):
-    pass
+    if request.user.is_authenticated:
+        groups = Groub.objects.all()
+
+    return render(request, 'product_detail.html', {'groups':groups})
+
