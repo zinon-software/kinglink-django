8796ef53c7cb5846216608242e0bea75d1c9db31
zinon <sawh2030@gmail.com>
2021-07-12 17:06:25 +0200

    Create comments models v2

--- whatsApp/models.py
+++ whatsApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
 
 # Create Group models here.
 
@@ -46,17 +45,11 @@ class Groub(models.Model):
 
 # Create comments models here.
 
-User = get_user_model()
-
 class Comment(models.Model):
     sender = models.ForeignKey(User, null=True, blank=True, related_name='sent_comment', on_delete=models.CASCADE)
     group = models.ForeignKey(Groub, null=True, blank=True, related_name='post_group', on_delete=models.CASCADE)
-    message = models.TextField(null=True, blank=True,)
+    message = models.TextField()
     created_dt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.sender.username
-    
-    
-    class Meta:
-        ordering = ("created_dt",)
+        return str(self.id)
