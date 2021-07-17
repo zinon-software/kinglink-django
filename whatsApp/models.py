8adb5690b3f9c7af57bc680835f50063547e7fb7
zinon <sawh2030@gmail.com>
2021-07-18 01:13:40 +0200

    v10

--- whatsApp/models.py
+++ whatsApp/models.py
@@ -22,8 +22,8 @@ class Groub(models.Model):
     link = models.CharField(max_length=255)
     activation = models.BooleanField(default=False, null=True, blank=True)
     created_dt = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, null=True, blank=True, related_name='Category_name', on_delete=models.CASCADE)
-    sections = models.ForeignKey(Sections, null=True, blank=True, related_name='Sections_name', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, default=1, null=True, blank=True, related_name='Category_name', on_delete=models.CASCADE)
+    sections = models.ForeignKey(Sections, default=1, null=True, blank=True, related_name='Sections_name', on_delete=models.CASCADE)
     views = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
