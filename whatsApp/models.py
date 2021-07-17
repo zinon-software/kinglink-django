3b2fe41a0cfb83715d2c4c19397a6eea0555ecee
zinon <sawh2030@gmail.com>
2021-07-18 00:00:21 +0200

    v6

--- whatsApp/models.py
+++ whatsApp/models.py
@@ -3,29 +3,7 @@ from django.contrib.auth.models import User
 
 # Create Group models here.
 
-CATEGORY = [
-    ('1', 'واتساب'),
-    ('2', 'تليجرام'),
-    ('3', 'انستقرام'),
-    ('4', 'يوتيوب'),
-    ('5', 'تطبيق'),
-    ('6', 'غير معروف'),
-]
 
-SECTIONS = [
-    ('1', 'الكل'),
-    ('2', 'إسلامية'),
-    ('3', 'أنمي'),
-    ('4', 'ترفيه'),
-    ('5', 'تعارف'),
-    ('6', 'وظائف'),
-    ('7', 'سياسة'),
-    ('8', 'تعليم'),
-    ('9', 'نكت'),
-    ('10', 'اعلانات'),
-    ('11', 'منوع'),
-    ('12', 'دعم'),
-]
 
 class Category(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
@@ -40,8 +18,6 @@ class Groub(models.Model):
     link = models.CharField(max_length=255)
     activation = models.BooleanField(default=False, null=True, blank=True)
     created_dt = models.DateTimeField(auto_now_add=True)
-    # category = models.CharField(max_length=2, choices=CATEGORY, default='1', null=True, blank=True)
-    # sections = models.CharField(max_length=2, choices=SECTIONS, default='1', null=True, blank=True)
     category = models.ForeignKey(Category, null=True, blank=True, related_name='Category_name', on_delete=models.CASCADE)
     sections = models.ForeignKey(Sections, null=True, blank=True, related_name='Sections_name', on_delete=models.CASCADE)
     views = models.IntegerField(default=0, null=True, blank=True)
