9de16de9e9ed70cdfbed6e6d8d6ceb55e54a26bd
zinon-software <business.zinon@gmail.com>
2021-12-12 03:57:16 -0800

    create notification within created new group

--- group/api/views.py
+++ group/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import permissions
 from account.models import Profile
 from group.models import Group
+from notification.models import Notification
 from .serializer import GroupSerializers, PostGroupSerializers
 
 # Create your views here.
@@ -43,7 +44,16 @@ class MyGroupListApiView(APIView):
 
         serializer = PostGroupSerializers(data=data)
         if serializer.is_valid():
-            serializer.save()
+            data = serializer.save()
+
+
+            # ارسال الاشعارات للمستخدمين بخصوص المنشور الجديد
+            followers_users = request.user.profile.followers.all()
+            followers_users = Profile.objects.filter(user__in=followers_users)
+            for follower_user in followers_users:
+                Notification.objects.create(sender=request.user.profile, receiver=follower_user, post=data, action= f"قام @{request.user.username} بالعجاب ب {data.titel}")
+            
+            
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -185,6 +195,8 @@ class LikeApiView(APIView):
             group_instance.likes.add(user)
             like = True
             post_id = '#like'+str(group_instance.id)
+            Notification.objects.create(sender=request.user.profile, receiver=group_instance.created_by, post=group_instance, action= f"قام @{request.user.username} بالعجاب ب {group_instance.titel}")
+        
            
         data = {'likes_count': group_instance.total_likes,'like':like,'post_id':post_id}
         return Response(data, status=status.HTTP_200_OK)
