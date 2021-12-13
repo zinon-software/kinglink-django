c720284f2a8a55045ccd36de6ff892c5fcf76cd6
zinon-software <business.zinon@gmail.com>
2021-12-13 04:09:12 -0800

    build the login code with account views

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from account.api.views import(
+	ObtainAuthToken,
 	UserFollowUnfollowApiView,
 	registration_view,
 	ProfileApiView,
 )
 
-from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'account'
 
@@ -13,7 +13,7 @@ urlpatterns = [
 	path('register', registration_view, name="register"),
 
 	
-	path('login', obtain_auth_token, name="login"), # -> see accounts/api/views.py for response and url info
+	path('login', ObtainAuthToken, name="login"), # -> see accounts/api/views.py for response and url info
 
 	path('follow-unfollow/<int:pk>',UserFollowUnfollowApiView.as_view(),name="follow-unfollow"),
 	path('<str:username>', ProfileApiView.as_view(), name='profile'),
